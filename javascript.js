6c9aaa5a73e7a9c762d4829d2caec1a8080da9a7
thach130801 <thach130801@gmail.com>
2024-05-22 17:58:16 +0700

    95% js logic implemented

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-// Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, 
-// so start by creating functions for the following items and testing them in your browser’s console.
-
 const calculator = {
     add: (a,b) => a+b,
     subtract: (a,b) => a-b,
@@ -15,92 +12,166 @@ const calculator = {
 }
 
 // A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5.
-// Create three variables for each of the parts of a calculator operation.
-// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 let num1, num2, operator;
-
 const operate = (operator, num1, num2) => operator(num1, num2)
 
-// Create the functions that populate the display when you click the number buttons.
-// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+let displayValue = '0';
 
-let displayValue = undefined;
-
-/// Add number function
+/// Add number input
 document.querySelectorAll('.digit-button').forEach((button) => {
-    button.addEventListener('click', () => {
-        const digit = button.textContent;
-        if (displayValue === undefined) {
-            displayValue = digit;
-        } else {
-            displayValue += digit;
-        }
-        updateDisplay();    
-    })
+    button.addEventListener('click', () => inputDigit(button.textContent));
 })
 
-// Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
-// You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘result’ to the operation.
-// This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
+function inputDigit(digit) {
+    if (displayValue === '0') {
+        displayValue = digit;
+    } else {
+        displayValue += digit;
+    }
+    updateDisplay();    
+}
 
 // Your calculator should not evaluate more than a single pair of numbers at a time.
 // Example: you press a number button (12), followed by an operator button (+), a second number button (7), and finally a second operator button (-).
 // Your calculator should then do the following: first, evaluate the first pair of numbers (12 + 7), second, display the result of that calculation (19), and finally, use that result (19) as the first number in your new calculation, along with the next operator (-).
 
 /// Add equals, clear function
-document.getElementById('clear').addEventListener('click', () => {
+document.getElementById('clear').addEventListener('click', () => CLEAR());
+function CLEAR() {
     num1 = undefined; num2 = undefined;
-    displayValue = undefined;
+    displayValue = '0';
     updateDisplay();
     clearPastDisplay();
-})  
-document.getElementById('equals').addEventListener('click', () => {
+}
+
+document.getElementById('equals').addEventListener('click', () => EQUALS());
+function EQUALS() {
     num2 = +displayValue;
-    evaluation();  
-})
+    evaluation(); 
+}
 
 /// Add operation
 document.querySelectorAll('.operator-button').forEach((button) => {
     button.addEventListener('click', () => {
         // handle that case
-        if (num1 !== undefined && displayValue !== undefined) {
+        if (num1 !== undefined && displayValue !== '0') {
             num2 = +displayValue;
             evaluation();
         }
         num1 = +displayValue;
         document.getElementById('past-display').textContent = `${num1} ${button.textContent}`;
-        
-        displayValue = undefined;
+        displayValue = '0';
 
-        switch(button.textContent) {
-            case '+':
-                operator = calculator.add;
-                break;
-            case '-':
-                operator = calculator.subtract;
-                break;
-            case 'x':
-                operator = calculator.multiply;
-                break;
-            case '/':
-                operator = calculator.divide;
-                break;
-        }
+        operator = whichOperation(button.textContent);
     });
 });
 
 function updateDisplay() {
     document.getElementById('current-display').textContent = displayValue;
 }
-
 function clearPastDisplay() {
     document.getElementById('past-display').textContent = '';
 }
-
 function evaluation() {
     displayValue = operate(operator, num1, num2);
-    Number.isInteger(displayValue) ? displayValue : displayValue.toFixed(3);
+    // case divide by 0
+    if (typeof displayValue === 'number' && !Number.isInteger(displayValue))
+        {
+            displayValue = Math.round(displayValue*1000)/1000;
+        }
     updateDisplay();
     clearPastDisplay();
 }
+
+// backspace, dot, negate
+document.getElementById('back').addEventListener('click', () => BACK());
+document.getElementById('dot').addEventListener('click', () => DOT());
+document.getElementById('neg').addEventListener('click', () => NEG());
+
+function BACK() {
+    if (displayValue.length === 1) {
+        displayValue = '0';
+    } else {
+        displayValue = displayValue.slice(0, -1);
+    }
+    updateDisplay();
+}
+function DOT() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.';
+    }
+    updateDisplay();
+}
+function NEG() {
+    displayValue += -1;
+    updateDisplay();
+}
+
+
+// add keyboard support
+const digitKeys = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  ];
+
+const operatorKeys = [
+    '+', '-', '*', '/',
+]
+
+const specialKeys = [
+    'Enter', 'Delete', 'Backspace', '.', '`',
+]
+
+document.addEventListener('keydown', handleKeyPress);
+
+function handleKeyPress(event) {
+    const keyPressed = event.key;
+
+    if (digitKeys.includes(keyPressed)) {
+        inputDigit(keyPressed);
+    }
+    else if (operatorKeys.includes(keyPressed)) {
+        if (num1 !== undefined && displayValue !== '0') {
+            num2 = +displayValue;
+            evaluation();
+        }
+        num1 = +displayValue;
+        document.getElementById('past-display').textContent = `${num1} ${keyPressed}`;
+        displayValue = '0';
+
+        operator = whichOperation(keyPressed);
+    }
+    else if (specialKeys.includes(keyPressed)) {
+        switch(keyPressed) {
+            case 'Enter':
+                EQUALS();
+                break;
+            case 'Delete':
+                CLEAR();
+                break;
+            case 'Backspace':
+                BACK();
+                break;
+            case '.':
+                DOT();
+                break;
+            case '`':
+                NEG();
+                break;
+        }
+    }
+}
+
+function whichOperation(key) {
+    switch(key) {
+        case '+':
+            return calculator.add;
+        case '-':
+            return calculator.subtract;
+        case '*':
+            return calculator.multiply;
+        case '/':
+            return calculator.divide;
+    }
+}
+  
